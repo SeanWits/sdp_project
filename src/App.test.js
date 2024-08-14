5f42b35c66e2de9dbb0ff2c56f6f1683f83cb912
SeanMz28 <2438634@students.wits.ac.za>
2024-08-14 09:23:52 +0200

    Testing set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+jest.spyOn(console, 'error').mockImplementation(() => {});
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders without crashing', () => {
+    render(<App />);
+  });
+
+  test('displays "O(n) Site" text', () => {
+    render(<App />);
+    const siteText = screen.getByText(/O\(n\) Site/i);
+    expect(siteText).toBeInTheDocument();
+  });
 });
