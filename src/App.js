e4fb722269cc7dd7f6180c86c2d8fa82b59a38b7
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-08-24 21:39:50 +0200

    New Firebase configs added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/compat/app";
+// Required for side-effects
+import "firebase/firestore";
 
 function App() {
   return (
+    
     <div className="App">
       O(n) Site
     </div>
