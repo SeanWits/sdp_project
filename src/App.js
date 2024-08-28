d2ec3e0fa4ef99580daf8ab103ecf8139b9a23cb
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-08-28 13:57:02 +0200

    New restaurant menu.js added with mock json file and menu.css all in pages folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
+import React, { useEffect, useState, createContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Menu from "./pages/menu";
+
+export const UserContext = createContext(null);
 
 function App() {
-  return (
-    <div className="App">
-      O(n) Site
-    </div>
-  );
+  const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={user}>
+            <Router>
+                <Routes>
+                    
+                    <Route path="/menu" element={<Menu />} />
+                    
+                </Routes>
+            </Router>
+        </UserContext.Provider>
+    );
+
 }
 
 export default App;
