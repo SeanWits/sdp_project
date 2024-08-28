47c696d54bdbe96ea0f16352f1dd4303baeafbc7
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-08-28 14:11:03 +0200

    .gitignore and firebase API hidden

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,28 @@
 import { initializeApp } from "firebase/app";
- import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
- import {getStorage} from "firebase/storage";
- //import { getAnalytics } from "firebase/analytics";
- import { getFirestore, doc, getDoc  } from "firebase/firestore";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4luwWT5h-locwYhH9_oWy7YQR_0HJ5iQ",
-    authDomain: "sdp-2024-project.firebaseapp.com",
-    databaseURL: "https://sdp-2024-project-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "sdp-2024-project",
-    storageBucket: "sdp-2024-project.appspot.com",
-    messagingSenderId: "795365387337",
-    appId: "1:795365387337:web:46e59d966ee13c40fd2398",
-    measurementId: "G-7LGZWXGWEK"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
- //const analytics = getAnalytics(app);
- const auth = getAuth(app)
-
- setPersistence(auth, browserLocalPersistence)
+const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
   .catch((error) => {
     console.error("Error setting persistence:", error);
   });
-  
- const db = getFirestore(app);
- const imgDB = getStorage(app);
- export {auth, db, doc, getDoc, imgDB}; 
+
+const db = getFirestore(app);
+const imgDB = getStorage(app);
+
+export { auth, db, doc, getDoc, imgDB };
+
+ 
