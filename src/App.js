70ac0bc079cd9d4af3c75b7e10c7955549102fae
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-09-02 09:06:41 +0200

    New menu page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState, createContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Menu from "./pages/menu";
+import Menu2 from "./pages/menu2";
 
 export const UserContext = createContext(null);
 
@@ -15,6 +16,7 @@ function App() {
                 <Routes>
                     
                     <Route path="/menu" element={<Menu />} />
+                    <Route path="/menu2" element={<Menu2 />} />
                     
                 </Routes>
             </Router>
