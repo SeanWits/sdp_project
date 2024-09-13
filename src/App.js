dda85c2e7b2ec4342d2c6ccaf21d578dab80ba2e
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-09-13 14:07:29 +0200

    App.js with commented Test.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Menu from "./pages/menu";
+import Menu2 from "./pages/menu2";
+import Menu3 from "./pages/menu3";
+import MenuInfo from "./pages/menuInfo";
+import Test from "./pages/Test";
+
+export const UserContext = createContext(null);
 import firebase from "firebase/compat/app";
 // Required for side-effects
 import "firebase/firestore";
@@ -13,6 +20,25 @@ import Login from './pages/Login/Login';
 import { UserProvider } from './utils/userContext';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={user}>
+            <Router>
+                <Routes>
+                    
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/menu3" element={<Menu3 />} />
+                    {/*<Route path="/Test" element={<Test />} />*/}
+                    {/*<Route path="/menuInfo" element={<MenuInfo />} />*/}
+                    <Route path="/restaurant-info/:id" element={<MenuInfo />} />
+                    {/*<Route path="/menu2" element={<Menu2 />} />  Remove this */}
+                    <Route path="/menu/:restaurantId" element={<Menu2 />} />
+                </Routes>
+            </Router>
+        </UserContext.Provider>
+    );
+
   return (
     <UserProvider>
       <Router>
