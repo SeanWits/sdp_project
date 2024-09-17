2b59567e37f4015d556cea9d0778f1e3aa2c32bf
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-17 13:28:45 +0200

    moved dietary restrictions implementation from app.jsx to pages/dietaryrestrictions.js

---
+++ src/pages/DietaryRestrictions.js
@@ -0,0 +1,51 @@
+import "./DietaryRestrictions.css";
+import { useState } from "react";
+import logo from "../assets/logo outline transparent.png";
+
+export default function DietaryRestrictions() {
+    const userPrefs = [];
+    const prefOptions = ["Halaal", "Vegan", "Vegetarian"];
+
+    const [allchecked, setAllChecked] = useState(userPrefs);
+    const [options, setOptions] = useState(prefOptions);
+
+    function handleChange(e) {
+        if (e.target.checked) {
+            setAllChecked([...allchecked, e.target.value]);
+        } else {
+            setAllChecked(allchecked.filter((item) => item !== e.target.value));
+        }
+    }
+
+    return (
+        <div className="DietaryRestrictions">
+            {/* <header className = "navbar">Navbar
+      <img src={logo} alt="Logo" className="App-logo"/>
+    </header> */}
+            <header className="App-header">Dietary Preferences</header>
+
+            <ul className="list-checkBox">
+                {options.map((prefs) => {
+                    return (
+                        <li>
+                            <label htmlFor={prefs} className="checkBox">
+                                <input
+                                    type="checkbox"
+                                    id={prefs}
+                                    value={prefs}
+                                    onChange={handleChange}
+                                    defaultChecked={allchecked.includes(prefs)}
+                                />
+                                {prefs}
+                            </label>
+                        </li>
+                    );
+                })}
+            </ul>
+
+            {/* <div> Your preferences are: {allchecked.join(" ,")}</div> */}
+
+            {/* <footer className="bottomBar">goodbye</footer> */}
+        </div>
+    );
+}
