9b5314e48e984b40f3e240588fa726653c085887
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-17 11:58:38 +0200

    added css for login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,46 +1,56 @@
-import React, { useState } from 'react';
-import { loginUser } from '../../utils/authFunctions';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { loginUser } from "../../utils/authFunctions";
+import { useNavigate } from "react-router-dom";
+import "./Login.css"
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await loginUser(email, password);
-      navigate('/'); // Redirect to home page after successful login
-    } catch (error) {
-      setError(error.message);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await loginUser(email, password);
+            navigate("/"); // Redirect to home page after successful login
+        } catch (error) {
+            setError(error.message);
+        }
+    };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{color: 'red'}}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+      <div id="login">
+        <section id="loging_logo">
+          <img id="logo_login" src={require("../../assets/logo outline transparent.png")}/>
+        </section>
+        <article id="login_article">
+            <section id="login_heading_section">
+                <h2>Login</h2>
+            </section>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <form id="form" onSubmit={handleSubmit}>
+                <input
+                    className="form_input"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                    />
+                <input
+                    className="form_input"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                    />
+                <button className="button" type="submit">Login</button>
+            </form>
+        </article>
+      </div>
+    );
 };
 
 export default Login;
