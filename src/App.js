5875378c307f9578cb49ccc5972faf52edb3310a
SeanMz28 <2438634@students.wits.ac.za>
2024-09-17 10:10:09 +0200

    Dashboard added with functionality

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Menu from "./pages/menu";
 import Menu2 from "./pages/menu2";
 import Menu3 from "./pages/menu3";
 import MenuInfo from "./pages/menuInfo";
+import Dashboard from './pages/Dashboard/Dashboard';
 
 import { UserProvider } from './utils/userContext';
 
@@ -28,6 +29,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/orders" element={<Orders />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/menu/:restaurantId" element={<Menu2 />} />
             <Route path="/menu/:restaurantId/:itemName" element={<Menu3 />} />
             <Route path="/restaurant-info/:id" element={<MenuInfo />} />
