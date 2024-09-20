6fb7d8b11b7df77a67decac5854c8a1a98bf5727
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-20 15:26:13 +0200

    implemented popup component

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Popup = ({ isOpen, onClose, children }) => {
+    if (!isOpen) return null;
+    return (
+        <div className="popup-overlay">
+            <div className="popup-content">{children}</div>
+            <button className="close-btn" onClick={onClose}>
+                Close
+            </button>
+        </div>
+    );
+};
+export default Popup;
