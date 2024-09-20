e956c3891751fdd85938ca8027656d4366a09fd3
ARTMashimbyi <2562071@students.wits.ac.za>
2024-09-20 17:42:42 +0200

    fixingReservationpage

--- src/pages/Reservation/ReservationPage/ReservationPage.js
+++ src/pages/Reservation/ReservationPage/ReservationPage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { db, collection, addDoc, serverTimestamp } from '../../../firebase'; // Import necessary Firebase functions
-import { styles } from '../styles
-0';
+import { styles } from '../styles';
 
 const ReservationPage = () => {
   const [date, setDate] = useState('');
