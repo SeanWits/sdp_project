9951a614e4be7f303f9128e3ea81951e58712de9
SeanMz28 <2438634@students.wits.ac.za>
2024-09-21 07:06:58 +0200

    Fixed RestaurantInfo name

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import Login from './pages/Login/Login';
 import Restaurant from "./pages/Restaurant/Restaurant";
 import Menu from "./pages/Menu/Menu";
 import Meal from "./pages/Meal/Meal";
-import MenuInfo from "./pages/MenuInfo/MenuInfo";
+import RestaurantInfo from "./pages/RestaurantInfo/RestaurantInfo";
 import Dashboard from './pages/Dashboard/Dashboard';
-
 import { UserProvider } from './utils/userContext';
+import RestaurantInfo from './pages/RestaurantInfo/RestaurantInfo';
 
 function App() {
   return (
@@ -30,7 +30,7 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/menu/:restaurantId" element={<Menu />} />
             <Route path="/menu/:restaurantId/:itemName" element={<Meal />} />
-            <Route path="/restaurant-info/:id" element={<MenuInfo />} />
+            <Route path="/restaurant-info/:id" element={<RestaurantInfo />} />
           </Routes>
         </div>
       </Router>
