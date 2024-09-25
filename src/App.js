4dd24f92d3729ac33879336e88c165a789110dfa
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-25 17:33:32 +0200

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
-import firebase from "firebase/compat/app";
 // Required for side-effects
 import "firebase/firestore";
 import Checkout from "./pages/Checkout/Checkout";
