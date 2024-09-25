48cee0c3e8c95ffaaa8c0ba5828c2b9312a4bbce
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-25 17:20:16 +0200

    formatting

--- src/pages/Reviews/AddReview.js
+++ src/pages/Reviews/AddReview.js
@@ -39,10 +39,10 @@ export function AddReview(restaurantID, mealID) {
 
     return (
         <>
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+            />
             <header id="reviews_header">
                 <span class="material-symbols-outlined">arrow_back</span>
                 <h2>Review Restaurant/Dining Hall</h2>
