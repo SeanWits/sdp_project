998a8891b0ff1338fff7b5aaf90c4db2d81bee5c
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-10-01 03:08:48 +0200

    added loading modal for reviews

--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -3,11 +3,13 @@ import Popup from "./Popup/Popup";
 import React, {useState, useContext, useEffect} from "react";
 import {UserContext} from "../../utils/userContext";
 import {AddReview} from "./AddReview";
+import LoadModal from "../../components/LoadModal/LoadModal";
 
 export function Reviews({restaurantID, mealID, onRatingChanged}) {
     const [reviews, setReviews] = useState([]);
     const [isPopupOpen, setIsPopupOpen] = useState(false);
     const {user} = useContext(UserContext);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchReviews = async () => {
@@ -29,6 +31,7 @@ export function Reviews({restaurantID, mealID, onRatingChanged}) {
                 console.error('Error fetching reviews:', error);
                 alert('Failed to fetch reviews');
             }
+            setTimeout(() => setLoading(false), 200);
         };
 
 
@@ -83,6 +86,7 @@ export function Reviews({restaurantID, mealID, onRatingChanged}) {
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
             />
+            <LoadModal loading={loading}/>
             <article id={"reviews_page"}>
                 <header className="header2" id="reviews_header">
                     <h2 id="h2_reviews" className="centre_no_margin">
@@ -104,39 +108,47 @@ export function Reviews({restaurantID, mealID, onRatingChanged}) {
                     </section>
                 </header>
                 <section id="review_section">
-                    {reviews.map((review, index) => (
-                        <div key={index} className="review-item">
-                            <section id="rating_section">
-                                <section
-                                    className="flex-direction-row"
-                                    id="rating_and_date_section"
-                                >
-                                    <h3 className="centre_no_margin">Rating:</h3>
-                                    <p id="rating_paragraph">{review.rating}</p>
-                                </section>
-                                <section
-                                    className="flex-direction-row"
-                                    id="date_posted_section"
-                                >
-                                    <h3 className="centre_no_margin">Date Posted:</h3>
-                                    <p id="date_posted_paragraph">
-                                        {review.dateCreated ? new Date(review.dateCreated._seconds * 1000).toLocaleDateString() : 'N/A'}
-                                    </p>
-                                </section>
-                            </section>
-                            <section id="review_text_section">
-                                <section id="review_text_heading_section">
-                                    <h3
-                                        className="centre_no_margin"
-                                        id="review_text_heading"
-                                    >
-                                        Review
-                                    </h3>
-                                </section>
-                                <p id="review_paragraph">{review.review}</p>
-                            </section>
-                        </div>
-                    ))}
+                    {!loading ? (
+                        reviews.length > 0 ? (
+                            reviews.map((review, index) => (
+                                <div key={index} className="review-item">
+                                    <section id="rating_section">
+                                        <section
+                                            className="flex-direction-row"
+                                            id="rating_and_date_section"
+                                        >
+                                            <h3 className="centre_no_margin">Rating:</h3>
+                                            <p id="rating_paragraph">{review.rating}</p>
+                                        </section>
+                                        <section
+                                            className="flex-direction-row"
+                                            id="date_posted_section"
+                                        >
+                                            <h3 className="centre_no_margin">Date Posted:</h3>
+                                            <p id="date_posted_paragraph">
+                                                {review.dateCreated ? new Date(review.dateCreated._seconds * 1000).toLocaleDateString() : 'N/A'}
+                                            </p>
+                                        </section>
+                                    </section>
+                                    <section id="review_text_section">
+                                        <section id="review_text_heading_section">
+                                            <h3
+                                                className="centre_no_margin"
+                                                id="review_text_heading"
+                                            >
+                                                Review
+                                            </h3>
+                                        </section>
+                                        <p id="review_paragraph">{review.review}</p>
+                                    </section>
+                                </div>
+                            ))
+                        ) : (
+                            <div className="no-reviews-message">
+                                <p>No Reviews.</p>
+                            </div>
+                        )) : (null)
+                    }
                 </section>
                 <Popup isOpen={isPopupOpen} onClose={togglePopup}>
                     <AddReview restaurantID={restaurantID} mealID={mealID} onReviewAdded={handleReviewAdded}/>
